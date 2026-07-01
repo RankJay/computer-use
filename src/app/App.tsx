ed1a25cce9e901584ac15f31fb828b2ae7e83bd6
RankJay <rankjay0101@gmail.com>
2026-07-01 13:26:03 +0530

    initial devel

---
+++ src/app/App.tsx
@@ -0,0 +1,18 @@
+﻿import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { AppQueryProvider } from "@/app/providers/QueryProvider";
+import { ControlCenter } from "@/features/control-center/ControlCenter";
+import { SettingsPage } from "@/features/settings/SettingsPage";
+
+export default function App() {
+  return (
+    <AppQueryProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ControlCenter />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AppQueryProvider>
+  );
+}
---
+++ src/features/settings/SettingsPage.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+export function SettingsPage(): ReactElement {
+  return (
+    <div className="box-border flex h-full min-h-dvh w-full flex-col gap-0 overflow-hidden rounded-none border-0 bg-[#0E0E0E] p-2 text-[#cdcdcd] shadow-none ring-0">
+      Settings Page
+    </div>
+  );
+}
